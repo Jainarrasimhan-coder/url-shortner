4222b5fd91427407451636b2e2b5fa445eb8e6aa
unknown <you@example.com>
2021-12-18 21:44:52 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const port = process.env.PORT || 3000
+    // const port = process.env.PORT || 3000
 mongoose.connect("mongodb://localhost:27017/myUrlShortner")
 
 const { UrlModel } = require('./models/urlshort')
